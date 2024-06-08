8eb96b7e71f839b0b8bc52c56edb9a8fb7e77a45
Zehra Akbulut <103004228+zehrakbulut@users.noreply.github.com>
2024-06-08 21:43:24 +0300

    Apide login işlemi tamamlandı.

---
+++ IdentityServer/ECommerce.IdentityServer/Controllers/LoginsController.cs
@@ -0,0 +1,35 @@
+﻿using ECommerce.IdentityServer.Dtos;
+using ECommerce.IdentityServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ECommerce.IdentityServer.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class LoginsController : ControllerBase
+	{
+		private readonly SignInManager<ApplicationUser> _signInManager;
+
+		public LoginsController(SignInManager<ApplicationUser> signInManager)
+		{
+			_signInManager = signInManager;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> UserLogin(UserLoginDto userLoginDto)
+		{
+			var result = await _signInManager.PasswordSignInAsync(userLoginDto.Username,userLoginDto.Password,false,false);
+            if (result.Succeeded)
+			{
+				return Ok("Giriş Başarılı");
+			}
+			else
+            {
+				return Ok("Kullanıcı Adı veya Şifre Hatalı");                
+            }
+        }
+	}
+}
