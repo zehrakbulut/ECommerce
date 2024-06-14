56ed3c1d69d9bd733725e6207584886ad9c3d10e
Zehra Akbulut <103004228+zehrakbulut@users.noreply.github.com>
2024-06-14 23:09:57 +0300

    Sipariş sayfa tasarımı düzenlendi.

--- Frontends/ECommerce.WebUI/ViewComponents/OrderViewComponents/_OrderSummaryComponentPartial.cs
+++ Frontends/ECommerce.WebUI/ViewComponents/OrderViewComponents/_OrderSummaryComponentPartial.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerce.WebUI.Services.BasketServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.WebUI.ViewComponents.OrderViewComponents
 {
     public class _OrderSummaryComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IBasketService _basketService;
+
+        public _OrderSummaryComponentPartial(IBasketService basketService)
         {
-            return View();
+            _basketService = basketService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var basketTotal = await _basketService.GetBasket();
+            var basketItems = basketTotal.BasketItems;
+            return View(basketItems);
         }
     }
 }
