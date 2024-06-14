4f44520e79ad9fb8245deb025d705b600760c6dd
Zehra Akbulut <103004228+zehrakbulut@users.noreply.github.com>
2024-06-15 00:01:20 +0300

    Kredi kart animasyonu tamamlandı.

---
+++ Frontends/ECommerce.WebUI/Controllers/PaymentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.directory1 = "Hediyem";
+            ViewBag.directory2 = "Ödeme Ekranı";
+            ViewBag.directory3 = "Kartla Ödeme";
+            return View();
+        }
+    }
+}
