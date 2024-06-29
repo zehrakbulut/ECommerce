afad52153f354bcfde2db43ba407300787c0ba1c
Zehra Akbulut <103004228+zehrakbulut@users.noreply.github.com>
2024-06-29 20:13:57 +0300

    User List Dto tamamlandı

---
+++ Frontends/ECommerce.WebUI/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        public async Task<IActionResult> UserList()
+        {
+            return View();
+        }
+    }
+}
--- IdentityServer/ECommerce.IdentityServer/Controllers/UsersController.cs
+++ IdentityServer/ECommerce.IdentityServer/Controllers/UsersController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
@@ -26,15 +28,22 @@ namespace ECommerce.IdentityServer.Controllers
         public async Task<IActionResult> GetUser()
         {
             var userClaim = User.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Sub);
-            var user=await _userManager.FindByIdAsync(userClaim.Value);
+            var user = await _userManager.FindByIdAsync(userClaim.Value);
             return Ok(new
             {
                 Id = user.Id,
                 Name = user.Name,
-                Surname= user.Surname,
+                Surname = user.Surname,
                 Email = user.Email,
                 UserName = user.UserName
             });
+        }
+
+        [HttpGet("GetAllUserList")]
+        public async Task<IActionResult> GetAllUserList()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return Ok(users);
         }
     }
 }
